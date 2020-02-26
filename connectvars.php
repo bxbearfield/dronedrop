82598e627eb5f934c242cc426575f9fe5a4dcff0
Briana Bearfield <xavierbearfield@gmail.com>
2020-02-25 20:18:05 -0500

    Configured connect vars for clearDB mysql heroku

--- connectvars.php
+++ connectvars.php
@@ -5,10 +5,15 @@ define( 'IG_APP_SECRET', 'bf3ebdf3ee25a141621126e897151061' );
 define( 'IG_APP_REDIRECT_URI', 'https://localhost/bikinibottombuddies/editprofile.php' );
 
 //Define database connection constants
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'bikinibottombuddies');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'pwd');
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'bikinibottombuddies');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', 'pwd');
+//mysql://bd5b0b761c4a16:e65ab998@us-cdbr-iron-east-04.cleardb.net/heroku_0814bc8535b6a0c?reconnect=true
+define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
+define('DB_NAME', 'heroku_0814bc8535b6a0c');
+define('DB_USER', 'bd5b0b761c4a16');
+define('DB_PASSWORD', 'e65ab998');
 
 
 // Define image application constants

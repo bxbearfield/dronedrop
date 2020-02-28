953fddd2406a2bf0db42d3953566fefaaac79ee0
Briana Bearfield <xavierbearfield@gmail.com>
2020-02-27 23:10:59 -0500

    checking server setup 10

--- viewprofile.php
+++ viewprofile.php
@@ -20,32 +20,37 @@ for ($i= 0; $i < sizeof($scriptName); $i++) {
 
 $command = array_values($requestURI);
 $command = explode('.', $command[0]);
+$first_name = isset($command[0]) ? mysqli_real_escape_string($command[0]) : '';
+$last_name =isset($command[1]) ? mysqli_real_escape_string($command[1]) : '';
+$user_id =isset($command[2]) ? mysqli_real_escape_string($command[2]) : '';
 
-$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-$query = "SELECT IG_AccessToken, IG_Private, user_id, picture FROM profile WHERE " . 
-        "first_name = '" . mysqli_real_escape_string($dbc, trim($command[0])) . "' AND last_name= '" . $command[1] . "' AND user_id = '" . $command[2] . "'";
+if ($first_name && $last_name && $user_id){
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $query = "SELECT IG_AccessToken, IG_Private, user_id, picture FROM profile WHERE " . 
+            "first_name = '" . mysqli_real_escape_string($dbc, trim($first_name)) . "' AND last_name= '" . $last_name . "' AND user_id = '" . $user_id . "'";
 
-$data = mysqli_query($dbc, $query)				
-or die(' SQL_SELECT_ERR: ' . mysqli_error($dbc) . ' SQL_ERR_NO.: ' . mysqli_errno($dbc) . ' QUERY_USED: '. $query );
-        
-if (mysqli_num_rows($data) == 1) {
-    //The user row was found so display the user data
-    $row = mysqli_fetch_array($data);
-    if(!$row['IG_Private']){
-        $showIgFeed = '';
-        $viewUser = $row['user_id'];
-        $viewPic = $row['picture'];
-        require_once('./igBasicAPI.php');
-        $params = array(
-            'get_code' => isset( $_GET['code'] ) ? $_GET['code'] : '',
-            'access_token' => $row['IG_AccessToken']
-        );
-        $ig = new instagram_basic_display_api( $params );
-        
-        if ($ig->getMedia()['data']){
-            foreach ($ig->getMedia()['data'] as $post) { 
-                // Display the score data
-                $showIgFeed .= 	'<img class="igImage" src="'.$post['media_url'].'" />';
+    $data = mysqli_query($dbc, $query)				
+    or die(' SQL_SELECT_ERR: ' . mysqli_error($dbc) . ' SQL_ERR_NO.: ' . mysqli_errno($dbc) . ' QUERY_USED: '. $query );
+            
+    if (mysqli_num_rows($data) == 1) {
+        //The user row was found so display the user data
+        $row = mysqli_fetch_array($data);
+        if(!$row['IG_Private']){
+            $showIgFeed = '';
+            $viewUser = $row['user_id'];
+            $viewPic = $row['picture'];
+            require_once('./igBasicAPI.php');
+            $params = array(
+                'get_code' => isset( $_GET['code'] ) ? $_GET['code'] : '',
+                'access_token' => $row['IG_AccessToken']
+            );
+            $ig = new instagram_basic_display_api( $params );
+            
+            if ($ig->getMedia()['data']){
+                foreach ($ig->getMedia()['data'] as $post) { 
+                    // Display the score data
+                    $showIgFeed .= 	'<img class="igImage" src="'.$post['media_url'].'" />';
+                }
             }
         }
     }

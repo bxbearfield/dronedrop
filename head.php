9e9a25270134900b1127f9f25969fc3429fd94d5
Briana Bearfield <xavierbearfield@gmail.com>
2020-03-10 23:30:42 -0400

    Final Draft Added Icon

--- head.php
+++ head.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0" />
 	<meta name="description" content="This is a free chatting website." />
 	
+	<link rel="icon" href="images/favicon.png">
 	<title>BBB | <?=$page_title?></title>
   	<link rel="stylesheet" type="text/css" href="./css/style.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
